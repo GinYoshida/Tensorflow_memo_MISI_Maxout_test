6b680e9d7212f9aee2d4f80ccc47b28a9dca02a0
GinYoshida <wood1969@hotmail.com>
2017-09-26 22:27:56 +0900

    Update Multi_input_Single_output_regression_with_Maxout.py

--- Multi_input_Single_output_regression_with_Maxout.py
+++ Multi_input_Single_output_regression_with_Maxout.py
@@ -9,10 +9,7 @@
 
 
 import tensorflow as tf
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
-from tensorflow.python import debug as tf_debug
 import random
 
 
